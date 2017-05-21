6620e6f45a9c6fe18e7ad74345c0b77c2776b250
Scot Nottish <jerry@orcon.net.nz>
2017-05-21 21:24:43 +1200

    Update basic.py

--- basic.py
+++ basic.py
@@ -4,6 +4,7 @@ import argparse
 import chardet
 import dns
 import os
+import requests
 import sys
 import urllib
 
@@ -11,6 +12,16 @@ def check_encoding(target_name):
 	target_url = "http://" + target_name
 	data = urllib.urlopen(target_url).read()
 	return chardet.detect(data)
+
+def check_status(target_name):
+	target_url = "http://" + target_name
+	response = requests.get(target_url, allow_redirects=False)
+	if response.status_code == 200:
+		return (200, None)
+	if response.status_code == 301 or response.status_code == 302:
+		return (response.status_code, response.headers['Location'])
+	if response.status_code == 404:
+		return (404, None)
 
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser(description="Check supplied domains for Redshield compatability.")
