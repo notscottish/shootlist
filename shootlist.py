55099ea26e74f15ee9d1a223256e72d81e942bdc
Scot Nottish <jerry@orcon.net.nz>
2017-06-09 12:37:32 +1200

    Create shootlist.py

--- shootlist.py
+++ shootlist.py
@@ -56,7 +56,7 @@ class Site(object):
 		self.dnsrecord += "\n"
         else:
             self.dnstype = "A"
-            self.dnsrecord = a.response.answer[0].__str__()
+            self.dnsrecord = a.response.answer[0].to_text()
         return
         
     def get_ip(self):
